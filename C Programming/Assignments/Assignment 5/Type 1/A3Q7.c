221f05325597f40f77ab37fbd5413691d3f1b46e
Anishka Dhande <anishkadhande8@gmail.com>
2025-08-01 00:47:57 +0530

    Add files via upload

---
+++ C Programming/Assignments/Assignment 5/Type 1/A3Q7.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+	fact();
+	}
+
+void fact()
+{
+	int n,i=1,fact=1;
+	printf("Enter Number\n");//5 =120
+	scanf("%d",&n);
+	int temp =n;
+	while(n>=i)
+	{  
+	  fact=	fact*i;
+	   i++;
+	}
+	
+	printf("Factorial of given number %d is %d ",temp,fact);
+}
