3e477ec367b0cfda5c2ce8fefc0fd26bdf5f0d33
Anishka Dhande <anishkadhande8@gmail.com>
2025-08-02 22:45:01 +0530

    Add files via upload

---
+++ C Programming/Assignments/Assignment 5/Type 1/A4Q3.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+	isPerfect();
+}
+void isPerfect()
+{
+	int num,i,end,start=1;
+	printf("Enter end :\n");
+	scanf("%d",&end);
+	printf("perfect numbers from 1 to %d are :",end);
+   for(num=start;num<=end;num++)
+   {
+   	 int sum=0;
+	for(i=1;i<num;i++)
+	{   
+	    if(num%i==0)
+	    {
+	    	sum=sum+i;
+		}
+	}
+	    if(sum==num)
+		printf("%d ",num);
+   }
+}
