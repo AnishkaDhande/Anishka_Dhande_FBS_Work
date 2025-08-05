eb89821746c916345961bbec1946e9cfff6507f8
Anishka Dhande <anishkadhande8@gmail.com>
2025-08-05 23:22:46 +0530

    Add files via upload

---
+++ C Programming/Assignments/Assignment 5/Type 2/A2Q1.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+float Amount();
+float price,discount;
+void main()
+{
+	
+	printf("Enter price :\n");
+	scanf("%f",&price);
+	float m=Amount();
+	printf("Price is %f",m);
+}
+float Amount()
+{
+	if(price<=1000)
+	{
+	  printf("Discount is 5 percent\n");
+	  discount =0.05*price;
+	}
+	else if(1000<price&& price<=5000)
+	{
+		printf("Discount is 10 percent\n");
+		discount =price*0.10;
+	}
+	else if(5000<price && price<=10000)
+	{
+		printf("Discount is 15 percent\n");
+		discount=0.15*price;
+	}
+	else 
+	{
+		printf("Discount is 20 percent\n");
+		discount=0.20*price;
+	}
+	 price =price-discount;
+	 return price;
+	 
+}
