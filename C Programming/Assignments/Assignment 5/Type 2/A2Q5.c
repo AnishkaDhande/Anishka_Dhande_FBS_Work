216dd3c03985ee5aaa9ec64e4f8c8fa0c119dec8
Anishka Dhande <anishkadhande8@gmail.com>
2025-08-09 22:32:29 +0530

    Add files via upload

---
+++ C Programming/Assignments/Assignment 5/Type 2/A2Q5.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+char ch;
+int discount,purchase_price;
+void price();
+void main()
+{
+    printf("Enter purchase Price\n ");
+	scanf("%d",&purchase_price);
+	fflush(stdin);
+	printf("customer is Student : y or N\n");
+	scanf("%c",&ch);
+	price();
+}
+void price(){
+	if(ch=='Y'||ch=='y')
+	{
+	if(purchase_price>500)
+		{
+			discount =purchase_price*0.20;
+			purchase_price =purchase_price - discount;
+			printf("The purchase price is %d ",purchase_price);
+		}
+    else
+		{
+			discount =0.10*purchase_price;
+			purchase_price =purchase_price - discount;
+			printf("The purchase_price is %d ",purchase_price);
+		}
+	}
+	else if(ch== 'n' ||ch =='N')
+	{
+	if(purchase_price>600)
+	{
+	 discount =0.15*purchase_price;
+	 purchase_price =purchase_price - discount;
+	 printf("The purchase price is %d ",purchase_price);	
+	}
+	else 
+	{
+		printf("The purchase price is %d ",purchase_price);
+	}
+    
+	} 
+	else 
+	printf("Invalid Input");
+}  
+
+
+
+
