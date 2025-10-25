2c98277ca293e959dc32f21968321c21116a62c0
Anishka Dhande <anishkadhande8@gmail.com>
2025-10-25 12:13:13 +0530

    Add files via upload

---
+++ Java/Command_line/Program1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Program1
+{
+	public static void main(String args[])
+	{
+		int a=Integer.parseInt(args[0]);
+		int b=Integer.parseInt(args[1]);
+		int c=a+b;
+		System.out.println("Addtion is "+c);
+		
+	}
+}
