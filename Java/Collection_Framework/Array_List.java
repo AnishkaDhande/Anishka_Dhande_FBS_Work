e43232002b179b64a8854866a213222b187a969b
Anishka Dhande <anishkadhande8@gmail.com>
2025-10-30 22:28:41 +0530

    Add files via upload

---
+++ Java/Collection_Framework/Array_List.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+public class Array_List {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList a1=new ArrayList();
+		a1.add(13);
+		a1.add(23);
+		a1.add(3);
+		a1.add(87);
+		a1.add(2);
+		a1.add(78);
+		
+		for(int i=0;i<a1.size();i++)
+		{
+			System.out.println(a1.get(i));
+		}
+		
+
+	}
+
+}
