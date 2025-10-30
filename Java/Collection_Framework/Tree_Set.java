eb5a23ab6f14f4103bd671f877dc6187cd7d7e91
Anishka Dhande <anishkadhande8@gmail.com>
2025-10-30 22:38:36 +0530

    Add files via upload

---
+++ Java/Collection_Framework/Tree_Set.java
@@ -0,0 +1,20 @@
+import java.util.TreeSet;
+public class Tree_Set {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      TreeSet a1=new TreeSet();
+      
+      a1.add(23);
+      a1.add(3);
+      a1.add(87);
+      a1.add(34);
+      a1.add(23);
+      a1.add(26);
+      
+      System.out.println("This is Tree set .sorting done by default");
+    	  System.out.println(a1);
+      
+	}
+
+}
